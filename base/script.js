9075d9143741081c103608dc282e4b24f61b2190
gongahkia <angryapplegravy@gmail.com>
2024-07-29 14:36:27 +0800

    i think im getting the hang of fullstack but wtf is this sia

--- base/script.js
+++ base/script.js
@@ -1,37 +1,37 @@
 import { extractFilmsEwc } from '../lib/eagleWingsCinematics.js';
-import { extractFilmsFgc } from '../lib/filmgardeCineplexes.js';
-import { extractFilmsGv } from '../lib/goldenVillage.js';
-import { extractFilmsTp } from '../lib/theProjector.js';
-import { extractFilmsWe } from '../lib/weCinemas.js';
+// import { extractFilmsFgc } from '../lib/filmgardeCineplexes.js';
+// import { extractFilmsGv } from '../lib/goldenVillage.js';
+// import { extractFilmsTp } from '../lib/theProjector.js';
+// import { extractFilmsWe } from '../lib/weCinemas.js';
 
 async function extractAllFilms() {
 
     const ewcUrl = "https://www.eaglewingscinematics.com.sg/movies#openModal";
-    const fgUrl = "https://fgcineplex.com.sg/movies";
-    const gvUrl = "https://www.gv.com.sg/GVMovies";
-    const tpUrl = "https://theprojector.sg/films";
-    const weUrl = `https://www.wecinemas.com.sg/buy-ticket.aspx?movieid=&date=${getCurrentDate()}`;
+    // const fgUrl = "https://fgcineplex.com.sg/movies";
+    // const gvUrl = "https://www.gv.com.sg/GVMovies";
+    // const tpUrl = "https://theprojector.sg/films";
+    // const weUrl = `https://www.wecinemas.com.sg/buy-ticket.aspx?movieid=&date=${getCurrentDate()}`;
 
     try {
 
         console.time("time to extract films");
 
-        const [ewcFilms, fgcFilms, gvFilms, tpFilms, weFilms] = await Promise.all([
+        const [ewcFilms] = await Promise.all([
             extractFilmsEwc(ewcUrl),
-            extractFilmsFgc(fgUrl),
-            extractFilmsGv(gvUrl),
-            extractFilmsTp(tpUrl),
-            extractFilmsWe(weUrl)
         ]);
+
+        // const [ewcFilms, fgcFilms, gvFilms, tpFilms, weFilms] = await Promise.all([
+        //     extractFilmsEwc(ewcUrl),
+        //     extractFilmsFgc(fgUrl),
+        //     extractFilmsGv(gvUrl),
+        //     extractFilmsTp(tpUrl),
+        //     extractFilmsWe(weUrl)
+        // ]);
 
         console.timeEnd("time to extract films");
 
         return {
             eagleWingsCinematicsArray: { ewcFilms },
-            filmGardeCineplexesArray: { fgcFilms },
-            goldenVillageArray: { gvFilms },
-            theProjectorArray: { tpFilms },
-            weCinemasArray: { weFilms }
         };
 
     } catch (error) {
